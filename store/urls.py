5a0c8f140f44207153fc30b879a848628accfdb4
MarianoZalazar <marianoz.1507@gmail.com>
2021-07-01 14:32:27 -0300

    Adding new templates and static files

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name="store"),
+    path('', views.index, name="index"),
+    path('store/', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+    path('product/', views.product, name="product"),
+    path('userlogin/', views.login, name="userlogin"),
+    path('register/', views.register, name="register"),
+    path('reset/', views.reset, name="reset"),
+    path('forgot/', views.forgot, name="forgot"),
+    
+    
 ]
--- store/views.py
+++ store/views.py
@@ -2,10 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    context = {}
+    return render(request, 'store/index.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'store/login.html', context)
+
+def register(request):
+    context = {}
+    return render(request, 'store/register.html', context)
+
+def reset(request):
+    context = {}
+    return render(request, 'store/reset.html', context)
+
+def forgot(request):
+    context = {}
+    return render(request, 'store/forgot.html', context)
+
 def store(request):
     context = {}
     return render(request, 'store/store.html', context)
-    
+
+def product(request):
+    context = {}
+    return render(request, 'store/product.html', context)
+
 def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
